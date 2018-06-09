2a76234743e1091c979ded64da3dfb72275cd11e
harryman201 <harrisonbateman@gmail.com>
2018-06-09 16:17:34 -0600

    Added assignment 7

---
+++ assignments/lesson-7/scripts/windchill.js
@@ -0,0 +1,11 @@
+var high = parseFloat(document.getElementById("high-temp").innerHTML);
+var low = parseFloat(document.getElementById("low-temp").innerHTML);
+var ws = parseFloat(document.getElementById("windspeed").innerHTML);
+
+var tavg = (high + low) / 2;
+
+var windchill = 35.74 + 0.6215 * tavg - 35.75 * Math.pow(ws, .16) + 0.4275 * tavg * Math.pow(ws, .16);
+
+
+
+document.getElementById("windchill").innerHTML = Math.round(windchill);
